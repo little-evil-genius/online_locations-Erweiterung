db9579e6a55de078b60d26c0efc2b35b714766a4
Lara <75126217+little-evil-genius@users.noreply.github.com>
2023-08-26 20:13:01 +0200

    Create online_location.lang.php

---
+++ inc/languages/deutsch_du/admin/online_location.lang.php
@@ -0,0 +1,50 @@
+<?php
+$l['online_location_permission'] = "Kann die Online Locations verwalten?";
+$l['online_location_manage'] = "Online Locations";
+$l['online_location_manage_header'] = "Online Locations verwalten";
+
+$l['online_location_manage_overview'] = "Eigene Online Locations";
+$l['online_location_manage_overview_desc'] = "In diesem Bereich kannst du die Information für die  Wer-ist-Wo/Online-Liste von eigene Seiten erstellen, verwalten und löschen.";
+$l['online_location_manage_overview_identification'] = "Name/Identifikator";
+$l['online_location_manage_overview_phpfile'] = "Name der PHP Datei";
+$l['online_location_manage_overview_parameter'] = "Parameter";
+$l['online_location_manage_overview_value'] = "Bezeichnung der Seite";
+$l['online_location_manage_overview_options'] = "Optionen";
+$l['online_location_manage_overview_options_edit'] = "Bearbeiten";
+$l['online_location_manage_overview_options_delete'] = "Löschen";
+$l['online_location_manage_no_elements'] = "Bisher wurden keine eigene Online Locations erstellt.";
+$l['online_location_manage_error_invalid'] = "Ungültige Option";
+
+$l['online_location_manage_overview_delete_notice'] = "Soll diese Online Location wirklich gelöscht werden?";
+$l['online_location_manage_overview_delete_logadmin'] = "Online Location gelöscht ";
+$l['online_location_manage_overview_delete_flash'] = "Die Online Location wurde erfolgreich gelöscht.";
+
+$l['online_location_manage_add'] = "Online Location hinzufügen";
+$l['online_location_manage_add_desc'] = "Hier kannst du eine neue Information für die  Wer-ist-Wo/Online-Liste von einer eigenen Seiten erstellen.";
+$l['online_location_manage_add_example'] = "Beispiel-Link: lists.php?action=charaview";
+$l['online_location_manage_add_example_desc'] = "Name: Charakterübersicht<br>PHP-Datei: lists<br>Parameter: action<br>Seitenbezeichnung: charaview";
+$l['online_location_manage_add_identification'] = "Name <em>*</em>";
+$l['online_location_manage_add_identification_desc'] = "Wie lautet der Name für die Online Location? Dieser wird später nur im ACP in der Übersicht angezeigt und dient als persönliche Orientierungshilfe.";
+$l['online_location_manage_add_phpfile'] = "PHP Datei <em>*</em>";
+$l['online_location_manage_add_phpfile_desc'] = "Wie lautet der Name der PHP-Datei? Gib nur den Namen und nicht noch die Endung .php an.";
+$l['online_location_manage_add_parameter'] = "Parameter";
+$l['online_location_manage_add_parameter_desc'] = "Wie lautet der Parameter nach dem ? im URL? Wenn keiner vorhanden ist, dann kann dieses Feld freigelassen werden.";
+$l['online_location_manage_add_value'] = "Seitenbezeichnung";
+$l['online_location_manage_add_value_desc'] = "Wie lautet der Wert vom Parameter? Die Bezeichnung nach dem Parameter und dem =. Wenn kein Paramter vorhanden ist, kann dieses Feld freigelassen werden.";
+$l['online_location_manage_add_location_name'] = "Anzeige in der Wer-ist-Wo/Online-Liste <em>*</em>";
+$l['online_location_manage_add_location_name_desc'] = "Trage hier ein, was in der Wer-ist-Wo/Online-Liste angezeigt werden soll. HTML ist möglich.";
+$l['online_location_manage_add_button'] = "Online Location speichern";
+$l['online_location_manage_add_error_identification'] = "Du hast keinen Namen angegeben.";
+$l['online_location_manage_add_error_phpfile'] = "Du hast keine PHP Datei angegeben.";
+$l['online_location_manage_add_error_location_name'] = "Du hast nichts eingetragen für die Anzeige in der Wer-ist-Wo/Online-Liste.";
+$l['online_location_manage_add_error_parameter_value'] = "Du hast einen Parameter angegeben, aber keinen passenden Wert (Seitenbezeichnung).";
+$l['online_location_manage_add_error_value_parameter'] = "Du hast eine Seitenbezeichnung angegeben, aber keinen Parameter.";
+$l['online_location_manage_add_logadmin'] = "Eine neue Online Location wurde hinzugefügt ";
+$l['online_location_manage_add_flash'] = "Die Informationen für die Online Location wurde erfolgreich erstellt.";
+
+$l['online_location_manage_edit'] = "Online Location bearbeiten";
+$l['online_location_manage_edit_desc'] = "Hier kannst du die Information für die  Wer-ist-Wo/Online-Liste von einer eigenen Seiten bearbeiten.";
+$l['online_location_manage_edit_container'] = "Online Location: {1}";
+$l['online_location_manage_edit_button'] = "Änderungen speichern";
+$l['online_location_manage_edit_logadmin'] = "Online Location geändert ";
+$l['online_location_manage_edit_flash'] = "Die Informationen für die Online Location wurde erfolgreich geändert.";
