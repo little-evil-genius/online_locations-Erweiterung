d44373cedc5136164d47a4d85798a9ca68316107
Lara <75126217+little-evil-genius@users.noreply.github.com>
2023-08-27 13:07:17 +0200

    Update online_location.lang.php

--- inc/languages/deutsch_du/admin/online_location.lang.php
+++ inc/languages/deutsch_du/admin/online_location.lang.php
@@ -4,7 +4,7 @@ $l['online_location_manage'] = "Online Locations";
 $l['online_location_manage_header'] = "Online Locations verwalten";
 
 $l['online_location_manage_overview'] = "Eigene Online Locations";
-$l['online_location_manage_overview_desc'] = "In diesem Bereich kannst du die Information für die  Wer-ist-Wo/Online-Liste von eigene Seiten erstellen, verwalten und löschen.";
+$l['online_location_manage_overview_desc'] = "In diesem Bereich kannst du die Information für die Wer-ist-Wo/Online-Liste von eigene Seiten erstellen, verwalten und löschen.";
 $l['online_location_manage_overview_identification'] = "Name/Identifikator";
 $l['online_location_manage_overview_phpfile'] = "Name der PHP Datei";
 $l['online_location_manage_overview_parameter'] = "Parameter";
@@ -12,7 +12,7 @@ $l['online_location_manage_overview_value'] = "Bezeichnung der Seite";
 $l['online_location_manage_overview_options'] = "Optionen";
 $l['online_location_manage_overview_options_edit'] = "Bearbeiten";
 $l['online_location_manage_overview_options_delete'] = "Löschen";
-$l['online_location_manage_no_elements'] = "Bisher wurden keine eigene Online Locations erstellt.";
+$l['online_location_manage_no_elements'] = "Bisher wurden keine eigenen Online Locations erstellt.";
 $l['online_location_manage_error_invalid'] = "Ungültige Option";
 
 $l['online_location_manage_overview_delete_notice'] = "Soll diese Online Location wirklich gelöscht werden?";
@@ -30,19 +30,19 @@ $l['online_location_manage_add_phpfile_desc'] = "Wie lautet der Name der PHP-Dat
 $l['online_location_manage_add_parameter'] = "Parameter";
 $l['online_location_manage_add_parameter_desc'] = "Wie lautet der Parameter nach dem ? im URL? Wenn keiner vorhanden ist, dann kann dieses Feld freigelassen werden.";
 $l['online_location_manage_add_value'] = "Seitenbezeichnung";
-$l['online_location_manage_add_value_desc'] = "Wie lautet der Wert vom Parameter? Die Bezeichnung nach dem Parameter und dem =. Wenn kein Paramter vorhanden ist, kann dieses Feld freigelassen werden.";
-$l['online_location_manage_add_location_name'] = "Anzeige in der Wer-ist-Wo/Online-Liste <em>*</em>";
+$l['online_location_manage_add_value_desc'] = "Wie lautet der Wert vom Parameter? Die Bezeichnung nach dem Parameter und dem =. Wenn kein Parameter vorhanden ist, kann dieses Feld freigelassen werden.";
+$l['online_location_manage_add_location_name'] = "Anzeigetext in der Wer-ist-Wo/Online-Liste <em>*</em>";
 $l['online_location_manage_add_location_name_desc'] = "Trage hier ein, was in der Wer-ist-Wo/Online-Liste angezeigt werden soll. HTML ist möglich.";
 $l['online_location_manage_add_button'] = "Online Location speichern";
 $l['online_location_manage_add_error_identification'] = "Du hast keinen Namen angegeben.";
 $l['online_location_manage_add_error_phpfile'] = "Du hast keine PHP Datei angegeben.";
-$l['online_location_manage_add_error_location_name'] = "Du hast nichts eingetragen für die Anzeige in der Wer-ist-Wo/Online-Liste.";
+$l['online_location_manage_add_error_location_name'] = "Du hast nichts eingetragen für den Anzeigetext in der Wer-ist-Wo/Online-Liste.";
 $l['online_location_manage_add_error_value_parameter'] = "Du hast eine Seitenbezeichnung angegeben, aber keinen Parameter.";
 $l['online_location_manage_add_logadmin'] = "Eine neue Online Location wurde hinzugefügt ";
 $l['online_location_manage_add_flash'] = "Die Informationen für die Online Location wurde erfolgreich erstellt.";
 
 $l['online_location_manage_edit'] = "Online Location bearbeiten";
-$l['online_location_manage_edit_desc'] = "Hier kannst du die Information für die  Wer-ist-Wo/Online-Liste von einer eigenen Seiten bearbeiten.";
+$l['online_location_manage_edit_desc'] = "Hier kannst du die Information für die  Wer-ist-Wo/Online-Liste von einer eigenen Seite bearbeiten.";
 $l['online_location_manage_edit_container'] = "Online Location: {1}";
 $l['online_location_manage_edit_button'] = "Änderungen speichern";
 $l['online_location_manage_edit_logadmin'] = "Online Location geändert ";
